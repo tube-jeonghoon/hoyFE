d0c33444ecf696575f98fa07c11200dff738372d
jeonghoon-tube <shu7081@gmail.com>
2023-08-22 17:01:12 +0900

    viewOtherGroup 스타일 수정

--- src/pages/viewOtherGroup/index.tsx
+++ src/pages/viewOtherGroup/index.tsx
@@ -3,7 +3,7 @@ import OtherTodo from '@/components/otherTodo';
 
 const ViewOtherGroup = () => {
   return (
-    <div className="px-[5rem] py-[3.75rem]">
+    <div className="px-[5rem] py-[3.75rem] overflow-y-auto h-[48rem]">
       <div className="cards grid grid-cols-3 gap-x-[3.12rem] gap-y-[3.75rem]">
         <OtherTodo />
         <OtherTodo />
